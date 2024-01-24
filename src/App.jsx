44b8e7fa5cf9ec28b64f9d5ad232c2b04bfd2b70
Rob Hess <hessro@oregonstate.edu>
2024-01-24 15:29:00 -0800

    Add code from 1/24 lecture.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import VideoPlayer from './VideoPlayer'
 
@@ -8,16 +8,29 @@ import './App.css'
 
 export default function PhotosApp() {
     const [ videoIsPlaying, setVideoIsPlaying ] = useState(false)
+    const counterRef = useRef(0)
+    const firstCatRef = useRef(null)
+    const secondCatRef = useRef(null)
+    const thirdCatRef = useRef(null)
+
+    function scrollToRef(ref) {
+        ref.current.scrollIntoView({
+            block: "center",
+            inline: "center",
+            behavior: "smooth"
+        })
+    }
+
     return (
         <>
             <nav>
-                <button>
+                <button onClick={() => scrollToRef(firstCatRef)}>
                     Go to first cat
                 </button>
-                <button>
+                <button onClick={() => scrollToRef(secondCatRef)}>
                     Go to second cat
                 </button>
-                <button>
+                <button onClick={() => scrollToRef(thirdCatRef)}>
                     Go to third cat
                 </button>
                 <button>
@@ -25,13 +38,23 @@ export default function PhotosApp() {
                 </button>
             </nav>
             <main>
-                <div>
+                <p>counterRef value: {counterRef.current}</p>
+                <button onClick={() => {
+                    counterRef.current += 1
+                    console.log("== counterRef.current:", counterRef.current)
+                }}>Update counter</button>
+                <button onClick={() => {
+                    const width = firstCatRef.current.offsetWidth
+                    const height = firstCatRef.current.offsetHeight
+                    console.log(`== Dimensions of the cat are ${width} x ${height}px`)
+                }}>Measure first cat</button>
+                <div ref={firstCatRef}>
                     <img src="http://placekitten.com/480/480?image=1" />
                 </div>
-                <div>
+                <div ref={secondCatRef}>
                     <img src="http://placekitten.com/480/480?image=2" />
                 </div>
-                <div>
+                <div ref={thirdCatRef}>
                     <img src="http://placekitten.com/480/480?image=3" />
                 </div>
                 <div>
